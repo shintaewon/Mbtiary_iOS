0d8db7af8b081ed0d65f1d5594e63a069fe9f7f3
Taewon Shin <tlsxodnjs147@gmail.com>
2022-10-05 18:29:33 +0900

    Initial Commit

---
+++ MBTIARY_Ver_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MBTIARY_Ver_1
+//
+//  Created by 신태원 on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
